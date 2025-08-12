8b865a8276e0f084fe1e0f872e224bf86c83c62b
AgustinHop <agibanez@hopenvios.com.ar>
2025-08-12 19:24:34 -0300

    Add global API config and update next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client']
+  },
+  typescript: {
+    ignoreBuildErrors: false
+  }
+}
 
 module.exports = nextConfig
